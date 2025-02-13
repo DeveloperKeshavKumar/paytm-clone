22e0039c55fb49ba43c1079c5294d1f33efd014c
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-02-13 22:03:52 +0530

    add required packages

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/index.routes.js');
+require('./config/db.js').connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/', router);
+app.use(express.json());
+app.use('/api/v1', router);
 
 
 app.listen(PORT, ()=>console.log(`Backend server started at http://localhost:${PORT}`));
