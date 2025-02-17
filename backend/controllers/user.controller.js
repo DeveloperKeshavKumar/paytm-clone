e548b9bbd0008adade1f1ff3efbd3f70a2747e8e
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-02-17 23:42:33 +0530

    add update user and get all users controllers

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -86,4 +86,59 @@ module.exports.getDetails = async (req, res, next) => {
         next(error);
     }
 }
-    
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+module.exports.updateDetails = async (req, res, next) => {
+    try {
+
+        const result = updateBody.safeParse(req.body);
+
+        if (!result.success) {
+            res.status(411).json({ message: "Invalid inputs" });
+        }
+
+        const { name, password } = req.body;
+        const existingUser = User.findById({ _id: req.userId });
+
+        if (!existingUser) {
+            res.status(403).json({ message: "Not authorized to update details" });
+        }
+
+        if (password.length < 8) {
+            res.status(411).json({ message: "Password too small" });
+        }
+
+        const passwordHash = await User.createHash(password);
+        existingUser.updateOne({ _id: req.userId }, { name, password: passwordHash });
+
+        return res.status(200).json({ message: "User updated successfully" });
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+
+        const filter = req.query.filter || '';
+        const users = await User.find({
+            name: { "$regex": filter }
+        })
+
+        return res.status(200).json({
+            message: "All users fetched", 
+            users: users.map(user => ({
+                email: user.email,
+                name: user.name
+            }))
+        })
+    } catch (error) {
+        next(error);
+    }
+}
