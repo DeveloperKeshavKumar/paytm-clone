2dcb0ef8d7bd8357175d1f91666bb7dac8ecd50c
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-02-17 23:42:57 +0530

    add edit user details and get all users routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { signIn, signUp, signOut, getDetails } = require('../controllers/user.controller.js');
+const { signIn, signUp, signOut, getDetails, updateDetails, getAllUsers } = require('../controllers/user.controller.js');
 const { authMiddleware } = require('../middlewares/auth.middleware.js');
 
 router.get('/', authMiddleware, getDetails);
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.delete('/signout', signOut);
+router.put('/', authMiddleware, updateDetails);
+router.get('/all', getAllUsers);
 
 
 module.exports = router;
