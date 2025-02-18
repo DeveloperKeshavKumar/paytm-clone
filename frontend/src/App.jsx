688efae0e4d461d382250db7d04e148f0012b7b8
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-02-19 01:28:22 +0530

    add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { Outlet, Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-    <div className='text-2xl font-bold bg-red-300'>Hello</div>
+      <Routes>
+        <Route path='/' element={<Outlet />} >
+          <Route index element={<>HOMEPAGE</>} />
+          <Route path='/dashboard' element={<>DASHBOARD</>} />
+          <Route path='/signup' element={<>Signup</>} />
+          <Route path='/signin' element={<>Signin</>} />
+          <Route path='/edit' element={<>Edit User</>} />
+          <Route path='/transfer' element={<>Transfer Fund</>} />
+          <Route path="*" element={<>404 Page Not Found</>} />
+        </Route>
+      </Routes>
     </>
   )
 }
