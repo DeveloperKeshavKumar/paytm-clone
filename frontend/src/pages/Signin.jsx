b89ef03391a0b8cffded3b41d1f25aaeb4312f68
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-02-25 21:23:57 +0530

    correct inputbox imports

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,7 +1,7 @@
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
-import { InputBox } from "../components/Inputbox"
+import { Inputbox } from "../components/Inputbox"
 import { SubHeading } from "../components/SubHeading"
 
 export const Signin = () => {
@@ -9,14 +9,14 @@ export const Signin = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
             <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-                <Heading label={"Sign in"} />
+                <Heading label={"Sign In"} />
                 <SubHeading label={"Enter your credentials to access your account"} />
-                <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
-                <InputBox placeholder="123456" label={"Password"} />
+                <Inputbox placeholder="harkirat@gmail.com" label={"Email"} />
+                <Inputbox placeholder="123456" label={"Password"} />
                 <div className="pt-4">
                     <Button label={"Sign in"} />
                 </div>
-                <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+                <BottomWarning label={"Don't have an account?"} buttonText={"Signup"} to={"/signup"} />
             </div>
         </div>
     </div>
