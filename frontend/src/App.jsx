6723b2077bd4c5075255f0a685a75a95b3ee1bc2
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-02-25 21:25:05 +0530

    add fund transfer page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Route, Routes } from "react-router-dom"
-import Signup from "./pages/Signup.jsx"
-import Signin from "./pages/Signin.jsx"
+import { Signup } from "./pages/Signup.jsx"
+import { Signin } from "./pages/Signin.jsx"
+import { Transfer } from "./pages/Transfer.jsx"
 
 function App() {
 
@@ -10,10 +11,10 @@ function App() {
         <Route path='/' element={<Outlet />} >
           <Route index element={<>HOMEPAGE</>} />
           <Route path='/dashboard' element={<>DASHBOARD</>} />
-          <Route path='/signup' element={<Signup/>} />
-          <Route path='/signin' element={<Signin/>} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
           <Route path='/edit' element={<>Edit User</>} />
-          <Route path='/transfer' element={<>Transfer Fund</>} />
+          <Route path='/transfer' element={<Transfer />} />
           <Route path="*" element={<>404 Page Not Found</>} />
         </Route>
       </Routes>
