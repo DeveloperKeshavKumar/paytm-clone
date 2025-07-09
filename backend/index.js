69cd63c77516735aeda728565596514efc537210
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-07-09 12:09:30 +0530

    update cors for production

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,15 @@ require('./config/db.js').connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://gorillapay.onrender.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/v1', router);
 
 
-app.listen(PORT, ()=>console.log(`Backend server started at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Backend server started at http://localhost:${PORT}`));
